e55cc2948d2ea7ddacc87f4274771670d034ee10
Levin Heinrich <levin.heinrich@stud.htw-dresden.de>
2025-06-06 13:07:57 +0200

    Vue Erklärung aktualisiert

--- src/frontend/shareshop_4b/src/components/ProjektEinführung.vue
+++ src/frontend/shareshop_4b/src/components/ProjektEinführung.vue
@@ -1,7 +1,7 @@
 <!-- src/components/ProjektEinführung.vue -->
 <template>
   <div>
-    <h1>ShareShop - Projektstruktur & Start</h1>
+      <h1>ShareShop - Projektstruktur & Start</h1>
     <p>Willkommen im ShareShop Vue-Projekt!</p>
 
     <section>
@@ -60,20 +60,31 @@
       <p>Diese Werkzeuge erlauben es dir, die Anwendung zu bauen und lokal im Browser auszuführen.</p>
     </section>
 
-    <section>
-      <h2>Wie starte ich die App lokal?</h2>
-      <ol>
-        <li>Im Projektverzeichnis öffnest du ein Terminal (z.B. in VSCode).</li>
-        <li>Du musst dich in diesem Verzeichnis befinden: <code>HTWD-SE-SHARESHOP_4B/src/frontend/shareshop_4b</code></li>
-        <li>Führe den Befehl <code>npm install</code> aus, um alle Abhängigkeiten zu installieren.</li>
-        <li>Starte den Entwicklungsserver mit <code>npm run dev</code>.</li>
-        <li>Öffne dann im Browser die angezeigte Adresse.</li>
-        <li>Nach einer Änderung Code speichern -> Änderung wird direkt angezeigt</li>
-        <li>Zum Abbrechen / Beenden <code>strg + c</code></li>
-      </ol>
-      <p>Jetzt kannst du die App live lokal testen und Änderungen werden automatisch aktualisiert.</p>
+     <section>
+      <h2>Hinweise!</h2>
+
+      <div>
+        <h3>Sprachen, die wir in .vue Dateien schreiben.</h3>
+        <ul>
+          <li><strong>HTML</strong> (mit Vue-Erweiterungen) <strong>im Bereich <code>&lt;template&gt;</code></strong></li>
+          <li><strong>Javascript im Bereich <code>&lt;script&gt;</code></strong></li>
+          <li><strong>CSS im Bereich <code>&lt;style&gt;</code></strong></li>
+        </ul>
+      </div>
+
+      <div>
+        <h3>Erklärung von zur Funktionalität von App.vue und den Komponenten.</h3>
+        <ul>
+          <li>Die <code>App.vue</code> ruft die einzelnen Komponenten nur auf.</li>
+          <li>Das heißt, dass der wesentliche Code - also Inhalte, Logik und Darstellung - wird in den jeweiligen Komponenten geschrieben.</li>
+          <li><strong>App.vue</strong> dient nur als Einstiegspunkt und Strukturgeber für die ganze App.</li>
+          <li>Es soll also <strong>so wenig Code wie möglich in der <code>App.vue</code> stehen.</strong></li>
+          <li><strong>Hinweis:</strong> das <strong>Dashboard</strong> wird auch in einer eigenen Komponente erstellt (<code>Dashboard.vue</code>) und nicht in der <code>App.vue</code>.</li>
+        </ul>
+      </div>
     </section>
   </div>
+    
 </template>
 
 <script></script>
