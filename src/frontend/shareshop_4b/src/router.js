72c93b941deb532947f05f2080de195427774605
Levin Heinrich <levin.heinrich@stud.htw-dresden.de>
2025-06-22 03:21:07 +0200

    kleine Anpassung in router.js

--- src/frontend/shareshop_4b/src/router.js
+++ src/frontend/shareshop_4b/src/router.js
@@ -13,16 +13,8 @@ const routes = [
   { path: "/registrieren", component: CreateAccount },
   { path: '/settings', component: UserSettings },
   { path: "/listen", component: ListOverview },
-  {
-    path: "/neueliste/",
-    component: ListCreator,
-  },
-  {
-    component: List,
-    path: "/list/:id",
-    props: true,
-    // TODO: Parameter für Listen übergabe, welcher eine Neue Liste aufruft zum erstellen
-  },
+  { path: "/neueliste/", component: ListCreator },
+  { path: "/list/:id", component: List, props: true, },
 ];
 const router = createRouter({
   history: createWebHistory(),
