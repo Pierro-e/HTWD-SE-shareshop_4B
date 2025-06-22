8d386f4b5004222bbe87e68ced9055aad6295052
Levin Heinrich <levin.heinrich@stud.htw-dresden.de>
2025-06-23 01:46:16 +0200

    userDaten auch in LocalStorage gespeicert

--- src/frontend/shareshop_4b/src/App.vue
+++ src/frontend/shareshop_4b/src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import { ref, provide } from 'vue'
+import { ref, provide, onMounted } from 'vue'
 import axios from 'axios'
 
 export default {
@@ -17,28 +17,41 @@ export default {
       name: '',
     })
 
+    // User beim Start aus localStorage laden
+    onMounted(() => {
+      const storedUser = localStorage.getItem('user')
+      if (storedUser) {
+        try {
+          user.value = JSON.parse(storedUser)
+        } catch {
+          localStorage.removeItem('user')
+        }
+      }
+    })
+
     function setUser(userData) {
       user.value = userData
+      // User auch im localStorage speichern
+      localStorage.setItem('user', JSON.stringify(userData))
+    }
+
+    async function getUser(id) {
+      try {
+        const response = await axios.get(`http://141.56.137.83:8000/nutzer/by-id`, {
+          params: { id: id }
+        })
+        return response.data
+      } catch (error) {
+        console.error('Fehler beim Laden des Nutzers:', error)
+        throw error
+      }
     }
 
     provide('user', user)
     provide('setUser', setUser)
+    provide('getUser', getUser)
 
-  async function getUser(id) {
-    try {
-      const response = await axios.get(`http://141.56.137.83:8000/nutzer/by-id`, {
-        params: { id: id }
-      })
-      return response.data
-    } catch (error) {
-      console.error('Fehler beim Laden des Nutzers:', error)
-      throw error
-    }
-  
-  }
-  provide('getUser', getUser)
-
-    return {} 
+    return {}
   }
 }
 </script>
