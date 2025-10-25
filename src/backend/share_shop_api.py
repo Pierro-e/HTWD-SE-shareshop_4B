f7bab3a4bc146921e873a6be2ceaabda9b9a4507
Levin Heinrich <levin.heinrich@stud.htw-dresden.de>
2025-10-25 22:02:49 +0200

    API-Funktion um theme und color zu setzen

--- src/backend/share_shop_api.py
+++ src/backend/share_shop_api.py
@@ -392,20 +392,29 @@ def get_listen_by_nutzer(nutzer_id: int = Path(..., gt=0), db: Session = Depends
             status_code=404, detail="Keine Listen für diesen Nutzer gefunden")
     return listen
 
+
+
+
 # -- Funktion, in der die Ansichtseinstellungen des Nutzer geändert werden können ---
 @app.put("/nutzer_change/{nutzer_id}/theme_color", status_code=status.HTTP_200_OK)
-def change_theme_color(nutzer_id: int, theme: int = Body(...), color: int = Body(...), db: Session = Depends(get_db)):
+def change_theme_color(
+    nutzer_id: int = Path(..., gt=0),                   # muss >0 sein
+    theme: int = Body(..., ge=0, le=2),                 # theme 0–2  ge = greater equal, le = less equal
+    color: int = Body(..., ge=0, le=4),                 # color 0–4
+    db: Session = Depends(get_db)
+):
+    # Prüfen, ob der Nutzer existiert
     nutzer = db.query(Nutzer).filter(Nutzer.id == nutzer_id).first()
-
     if not nutzer:
         raise HTTPException(status_code=404, detail="Nutzer nicht gefunden")
 
+    # Werte aktualisieren
     nutzer.theme = theme
     nutzer.color = color
     db.commit()
     db.refresh(nutzer)
 
-    return nutzer       
+    return nutzer
 
 
 # --- Einheiten ---
