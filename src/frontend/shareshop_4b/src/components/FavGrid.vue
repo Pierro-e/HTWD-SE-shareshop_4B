c379971edc456d4ffb0deb912df29bc0ec69b329
Erik Wenke <erik.wenke@stud.htw-dresden.de>
2025-11-10 17:35:03 +0100

    get all other stuff again

---
+++ src/frontend/shareshop_4b/src/components/FavGrid.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="produkte-grid">
+    <FavItem v-for="f in favs" :fav="f"/>
+  </div>
+</template>
+
+<script>
+import FavItem from "./FavItem.vue";
+
+export default {
+  components: { FavItem },
+  props: {
+    favs: { type: Array },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/frontend/shareshop_4b/src/components/TextInput.vue
@@ -0,0 +1,19 @@
+<template>
+  <input
+    type="text"
+    :value="text"
+    :placeholder="placeholder"
+    maxlength="30"
+    @input="$emit('update:text', $event.target.value)"
+  />
+</template>
+<script>
+export default {
+  props: {
+    text: { type: String },
+    placeholder: {type: String},
+  },
+};
+</script>
+
+<style scoped></style>
