f71558138a83bf97156a60e160337609e3389ce5
Levin Heinrich <levin.heinrich@stud.htw-dresden.de>
2025-11-19 17:46:41 +0100

    ProductArchive-Komponente eingebaut

--- src/frontend/shareshop_4b/src/router.js
+++ src/frontend/shareshop_4b/src/router.js
@@ -10,6 +10,7 @@ import ProductDetail from "./components/ProductDetail.vue";
 import Einkauf from "./components/Einkauf.vue";
 import Favorites from "./components/Favorites.vue"
 import ListArchive from "./components/ListArchive.vue";
+import ProductArchive from "./components/ProductArchive.vue";
 
 const routes = [
   { path: "/", component: Login },
@@ -27,6 +28,7 @@ const routes = [
   },
   { path: "/list/:listenId/einkauf", component: Einkauf, props: true },
   { path: "/list/:list_id/archive", component: ListArchive, props: true },
+  { path: "/product/archive/:purchase_id", component: ProductArchive, props: true },
 ];
 const router = createRouter({
   history: createWebHistory(),
