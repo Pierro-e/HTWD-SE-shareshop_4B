0a89d96b08be8d5e4d9442bfd58cba9423c31e0c
Levin Heinrich <levin.heinrich@stud.htw-dresden.de>
2025-11-19 19:24:03 +0100

    ProductArchive grundlegend erstellt

--- src/frontend/shareshop_4b/src/components/ProductArchive.vue
+++ src/frontend/shareshop_4b/src/components/ProductArchive.vue
@@ -1,6 +1,67 @@
 <template>
-    <div class="list-archive">
-        <h2>Listenarchiv</h2>
-        <p>Hier werden alle archivierten Listen angezeigt.</p>
+  <div class="list-archive">
+
+    <AppHeader :title="`Einkaufsarchiv für\n${purchase_name}`" class="multiline-title">
+      <template #left>
+        <button @click="back_to_listArchive" class="button button-cancel back-button">
+          Zurück
+        </button>
+      </template>
+    </AppHeader>
+
+    <div v-if="loadingActive" class="loading">Laden...</div>
+    <div v-else-if="errorMessage" class="error">{{ errorMessage }}</div>
+
+    <div v-else-if="purchased_products.length === 0" class="info">
+      Es wurden noch keine Produkte eingekauft.
     </div>
+
+    <div v-else>
+      <ProductCard
+        v-for="product in purchased_products"
+        :key="product.id"
+        :product="product"
+      />
+    </div>
+
+  </div>
 </template>
+
+<script>
+import axios from 'axios';
+import AppHeader from './AppHeader.vue';
+import ProductCard from './ProductCard.vue';
+
+export default {
+  name: "ProductArchive",
+  props: ["purchase_id"],
+  components: { AppHeader, ProductCard },
+
+  data() {
+      return{
+          list_id: null,
+          purchase_name: "",
+          purchased_products: [],
+          loadingActive: true,
+          errorMessage: "",
+      }
+  },
+
+  methods: {
+    back_to_listArchive() {
+      this.$router.push(`/list/${this.list_id}/archive`);
+    }
+        
+
+  },
+
+  mounted() {
+    this.purchase_name = this.$route.query.purchase_name;
+    this.list_id = this.$route.query.list_id;
+    
+
+  }
+
+
+}
+</script>
