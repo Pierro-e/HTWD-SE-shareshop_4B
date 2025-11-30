cc2a904fc90102bbe23433116635534570c9bf10
Erik Wenke <erik.wenke@stud.htw-dresden.de>
2025-11-30 19:59:49 +0100

    Default Option hinzugefügt

--- src/frontend/shareshop_4b/src/components/input/SelectObjectArray.vue
+++ src/frontend/shareshop_4b/src/components/input/SelectObjectArray.vue
@@ -2,6 +2,7 @@
   <label>
     {{ label }}
     <select :value="choice" @change="onChange">
+      <option value="">Keine Angabe</option>
       <option
         v-for="(option, index) in options"
         :key="index"
