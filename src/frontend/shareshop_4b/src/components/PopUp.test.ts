d407d6f5eafb9f1196c811f68fa41111775377b2
Erik Wenke <erik.wenke@stud.htw-dresden.de>
2025-12-06 10:07:22 +0100

    UPDATE: erstes Beispiel für Unittes mit Browswer Mode

--- src/frontend/shareshop_4b/src/components/PopUp.test.ts
+++ src/frontend/shareshop_4b/src/components/PopUp.test.ts
@@ -1,18 +1,8 @@
 import PopUp from "./PopUp.vue";
-import { render } from "@testing-library/vue";
-import { describe, it, expect, test } from "vitest";
+import { render } from "vitest-browser-vue";
+import { expect, test } from "vitest";
 
-it("der Default-Name ist PopUp", () => {
-  const { getByRole } = render(PopUp);
-  const header = getByRole("heading");
-  console.log(header.attributes);
-});
-
-test("should work", ({ task }) => {
-  console.log(task.name);
-});
-
-test("math is easy", ({ task }) => {
-  console.log(task.name);
-  expect(2 + 7).toBe(9);
+test("Default-Name", async () => {
+  const { getByText } = render(PopUp);
+  await expect.element(getByText("PopUp")).toBeInTheDocument();
 });
