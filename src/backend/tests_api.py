e822fe1b3bb08abb2bb8e5688dee524769ecdfd3
Ahmad Alrmih <s86396@informatik.htw-dresden.de>
2026-01-18 17:04:52 +0100

    test erfolgreiches Abrufen des Nutzers nach ID

--- src/backend/tests_api.py
+++ src/backend/tests_api.py
@@ -58,3 +58,21 @@ def test_get_nutzer_all_success(mock_session_local):
     assert result[0].id == 1
     assert result[0].email == 'user1@example.com'
     assert result[1].id == 2
+
+
+@patch('share_shop_api.SessionLocal')
+def test_get_nutzer_by_id_success(mock_session_local):
+    """ Testet das erfolgreiche Abrufen eines Nutzers nach ID. """
+    # Arrange
+    mock_db = MagicMock()
+    mock_session_local.return_value = mock_db
+
+    mock_nutzer = create_mock_nutzer(1, 'user1@example.com', 'User1')
+    mock_db.query.return_value.filter.return_value.first.return_value = mock_nutzer
+
+    # Act
+    result = get_nutzer_by_id(1, db=mock_db)
+
+    # Assert
+    assert result.id == 1
+    assert result.email == 'user1@example.com'
