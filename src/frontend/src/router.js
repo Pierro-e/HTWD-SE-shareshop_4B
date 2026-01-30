5f343af7c889c04451f05e819581a6019848fc4d
Levin Heinrich <levin.heinrich@stud.htw-dresden.de>
2026-01-30 22:56:35 +0100

    fix

--- src/frontend/src/router.js
+++ src/frontend/src/router.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "./components/Login.vue";
-import ProjektEinführung from "./components/ProjektEinführung.vue";
 import CreateAccount from "./components/CreateAccount.vue";
 import ListOverview from "./components/ListOverview.vue";
 import ListCreator from "./components/ListCreator.vue";
@@ -15,7 +14,6 @@ import Finanzen from "./components/Finanzen.vue";
 
 const routes = [
   { path: "/", component: Login },
-  { path: "/einfuehrung", component: ProjektEinführung },
   { path: "/registrieren", component: CreateAccount },
   { path: "/settings", component: UserSettings },
   { path: "/listen", component: ListOverview },
