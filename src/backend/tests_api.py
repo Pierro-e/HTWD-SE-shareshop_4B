092acfb1e7dbdd949fb8e54a89129cb37c5d7bcc
Ahmad Alrmih <s86396@informatik.htw-dresden.de>
2026-01-30 15:15:08 +0100

    test: update fav-produkte

--- src/backend/tests_api.py
+++ src/backend/tests_api.py
@@ -13,6 +13,7 @@ with patch.dict(os.environ, {
     from share_shop_api import get_nutzer_all, get_nutzer_by_id, create_nutzer, get_produkte_all, get_produkt_by_id, create_produkt, get_fav_produkte_by_nutzer, get_nutzer_by_email, create_fav_produkt
     from share_shop_api import change_passwort, change_name, change_email
     from share_shop_api import delete_nutzer, get_eingekaufte_produkte, get_kostenaufteilung_empfaenger, delete_liste, create_liste, add_mitglied
+    from share_shop_api import update_fav_produkt
     from share_shop_api import NutzerCreate, ProduktCreate, ListeCreate
 
 # Hilfsfunktion zum Erstellen von Dummy-Nutzer-Objekten (als MagicMock mit Attributen)
@@ -585,3 +586,16 @@ def test_create_fav_produkt_success(mock_session_local):
 
     result = create_fav_produkt(1, body, db=mock_db)
     assert result.nutzer_id == 1
+
+@patch('share_shop_api.SessionLocal')
+def test_update_fav_produkt_success(mock_session_local):
+    mock_db = MagicMock()
+    mock_session_local.return_value = mock_db
+    mock_fav = create_mock_fav_produkt(1, 1, 'Apfel', 2.0)
+    mock_db.query.return_value.filter.return_value.first.return_value = mock_fav
+
+    body = MagicMock()
+    body.menge = 5
+
+    result = update_fav_produkt(1, 1, body, db=mock_db)
+    assert result.menge == 5
